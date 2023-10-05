37d4f284a8c26e5412d71384807a9a23e3540507
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-05 19:56:32 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
 /**
  * main - A c program that prints a line to the standerd error
  * Return: 1 (success)
  */
-int main(void)
-{
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r");
-	return (1);
+int main(void) {
+    const char *message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+    ssize_t len = strlen(message);
+    ssize_t bytes_written = write(2, message, len);
+
+    if (bytes_written == len) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
