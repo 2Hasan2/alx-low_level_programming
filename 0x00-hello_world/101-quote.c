db855eedb96c979b4c694589ab38d0b54f697d13
2Hasan2 <hr145310@gmail.com>
2023-10-05 10:14:02 -0700

    the last fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  * Return: 1 (success)
  */
 int main(void) {
-    const char *message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     ssize_t len = strlen(message);
     ssize_t bytes_written = write(2, message, len);
 
