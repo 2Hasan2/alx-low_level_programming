d3f66fe1a3151a134a58384ad82fb6731debbab8
2Hasan2 <hr145310@gmail.com>
2023-10-10 14:00:41 -0700

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,7 @@ void times_table(void)
                 printf("%3d", product);
 
             if (column < 9)
-                printf(", ");
+                printf(",");
         }
         printf("\n");
     }
