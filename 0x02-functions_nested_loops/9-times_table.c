dd8fa4198ac1e2d35b065d5071275c9cc7704bcc
2Hasan2 <hr145310@gmail.com>
2023-10-10 14:12:02 -0700

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,23 +6,23 @@
  */
 void times_table(void)
 {
-    int row, column, product;
+	int row, column, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (column = 0; column <= 9; column++)
-        {
-            product = row * column;
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
 
-            if (column == 0)
-                printf("0");
-            else
-                printf("%2d", product);
+			if (column == 0)
+				printf("0");
+			else
+				printf("%2d", product);
 
-            if (column < 9)
-                printf(", ");
-        }
-        printf("\n");
-    }
+			if (column < 9)
+				printf(", ");
+		}
+		printf("\n");
+	}
 }
 
