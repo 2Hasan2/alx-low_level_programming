38f55827cfc6ba41b42c5283cb84b44c8ffa7c7a
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-10 22:24:34 +0300

    `2-print_alphabet_x10.c`

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the english alphabet from a-z
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+	int y=0;
+
+	for (; y < 10; y++)
+	{
+
+		for (x = 'a'; x <='z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
