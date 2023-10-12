35e10467ee7e9205eb2f5bd356402889420dea96
2Hasan2 <hr145310@gmail.com>
2023-10-12 13:14:12 -0700

    2023-10-12 13:14:11

--- push.sh
+++ push.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+if [ -z "$1" ]; then
+  commit_message=$(date +"%Y-%m-%d %H:%M:%S")
+else
+  commit_message="$1"
+fi
+
+git pull
 git add .
-git commit -m 'fix'
+git commit -m "$commit_message"
 git push
+
