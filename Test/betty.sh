d4ac657b4cd8c8f647545344567bc5e7ff12812a
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-13 23:56:39 +0300

    Update betty.sh

--- Test/betty.sh
+++ Test/betty.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-# Check if betty is installed
-if ! type "betty" &>/dev/null; then
-    echo "Betty is not installed. Please install Betty before using this script."
-    exit 1
+# Check if a file was provided as an argument
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <c_file>"
+  exit 1
 fi
 
-# Check for input file
-if [ -z "$1" ]; then
-    echo "Usage: $0 <C_FILE>"
-    exit 1
-fi
-
+# Store the C file name
 c_file="$1"
 
-# Check if the input file exists
+# Check if the file exists
 if [ ! -f "$c_file" ]; then
-    echo "File not found: $c_file"
-    exit 1
+  echo "Error: File not found."
+  exit 1
 fi
 
-# Run betty to fix style errors and update the file
-betty -f "$c_file"
+# Apply Betty style fixes using 'sed'
+# You may need to customize this script for your specific needs.
+sed -i 's/[A-Z]/\L&/g' "$c_file" # Convert uppercase to lowercase
+sed -i 's/\([a-z]\)\([A-Z]\)/\1_\L\2/g' "$c_file" # Convert CamelCase to snake_case
+sed -i 's/.\{81\}/&\n/' "$c_file" # Break lines that exceed 80 characters
+sed -i 's/,,/,/g' "$c_file" # Remove multiple commas
+sed -i 's/\(\w\)\(\s*\)\([;=,)]\)/\1\3/g' "$c_file" # Remove spaces before certain operators
+
+# Add more 'sed' commands to fix other errors as needed
+
+echo "Betty errors in $c_file have been fixed."
