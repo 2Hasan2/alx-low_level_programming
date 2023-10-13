c70d1be4d9897e647cb8b55bfadc5d1d28a57c1b
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-13 23:52:13 +0300

    update

--- Test/betty.sh
+++ Test/betty.sh
@@ -1,47 +1,24 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <C-file>"
+
+# Check if betty is installed
+if ! type "betty" &>/dev/null; then
+    echo "Betty is not installed. Please install Betty before using this script."
+    exit 1
+fi
+
+# Check for input file
+if [ -z "$1" ]; then
+    echo "Usage: $0 <C_FILE>"
     exit 1
 fi
 
 c_file="$1"
 
+# Check if the input file exists
 if [ ! -f "$c_file" ]; then
     echo "File not found: $c_file"
     exit 1
 fi
 
-temp_file="temp_$c_file"
-cp "$c_file" "$temp_file"
-
-betty "$temp_file"
-
-if [ $? -ne 0 ]; then
-    echo "Betty found errors in the file. Please fix them manually."
-    rm "$temp_file"
-    exit 1
-fi
-
-sed -i 's/\t/  /g' "$temp_file"
-echo "" >>"$temp_file"
-sed -i 's/\bif(/if (/g' "$temp_file"
-sed -i 's/\bfor(/for (/g' "$temp_file"
-sed -i 's/\bwhile(/while (/g' "$temp_file"
-awk '/^\s*\w+\s+\w+\(.*\)\s*{/{print ""}1' "$temp_file" >"$temp_file.tmp"
-mv "$temp_file.tmp" "$temp_file"
-
-if ! grep -q "Holberton School" "$temp_file"; then
-    author_name="Your Name"
-    file_description="File description here"
-    echo "/*" >"$temp_file.tmp"
-    echo " * File: $c_file" >>"$temp_file.tmp"
-    echo " * Author: $author_name" >>"$temp_file.tmp"
-    echo " * Description: $file_description" >>"$temp_file.tmp"
-    echo " */" >>"$temp_file.tmp"
-    cat "$temp_file" >>"$temp_file.tmp"
-    mv "$temp_file.tmp" "$temp_file"
-fi
-
-mv "$temp_file" "$c_file"
-
-echo "Fixed $c_file to comply with Betty style."
+# Run betty to fix style errors and update the file
+betty -f "$c_file"
