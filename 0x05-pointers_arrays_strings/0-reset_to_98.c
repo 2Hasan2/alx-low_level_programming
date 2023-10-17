b51cdcac68f3434ae763ab92e3a4f589f5b160e8
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-17 18:04:16 +0300

    fix `0-reset_to_98.c` and edit `betty.sh`

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 /**
  * reset_to_98 : reset value of an integer to 98
  * @n : parameter targeted
- * Return : nothing 
- */ 
+ * Return : nothing
+ */
 
 void reset_to_98(int *n)
 {
--- Test/betty.sh
+++ Test/betty.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
+# Check if Betty is installed
+if ! command -v betty >/dev/null; then
+  echo "Betty is not installed. Please install Betty to use this script."
+  exit 1
+fi
+
 # Check if a file was provided as an argument
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <c_file>"
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <filename.c>"
   exit 1
 fi
 
-# Store the C file name
-c_file="$1"
-
-# Check if the file exists
-if [ ! -f "$c_file" ]; then
-  echo "Error: File not found."
+# Check if the provided file exists
+if [ ! -f "$1" ]; then
+  echo "File not found: $1"
   exit 1
 fi
 
-# Apply Betty style fixes using 'sed'
-# You may need to customize this script for your specific needs.
-sed -i 's/[A-Z]/\L&/g' "$c_file" # Convert uppercase to lowercase
-sed -i 's/\([a-z]\)\([A-Z]\)/\1_\L\2/g' "$c_file" # Convert CamelCase to snake_case
-sed -i 's/.\{81\}/&\n/' "$c_file" # Break lines that exceed 80 characters
-sed -i 's/,,/,/g' "$c_file" # Remove multiple commas
-sed -i 's/\(\w\)\(\s*\)\([;=,)]\)/\1\3/g' "$c_file" # Remove spaces before certain operators
+# Run Betty on the file
+betty "$1"
 
-# Add more 'sed' commands to fix other errors as needed
-
-echo "Betty errors in $c_file have been fixed."
+# Check if Betty suggested any fixes
+if [ -f "$1-errors" ]; then
+  echo "Betty found style issues in $1. Applying fixes..."
+  betty --fix "$1"
+  rm -f "$1-errors"
+else
+  echo "No style issues found in $1. Your code is Betty-compliant."
+fi
