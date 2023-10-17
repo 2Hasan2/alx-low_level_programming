64e5c18775c923b83c1e923d281ee5a995173e19
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-17 19:36:00 +0300

    add

---
+++ 0x05-pointers_arrays_strings/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char putChar[] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 7; i++)
+	{
+		putchar(putChar[i]);
+	}
+	putchar('\n');
+	return (0);
+}
