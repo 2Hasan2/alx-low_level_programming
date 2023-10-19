ce8faa7bdeb44883953b4bd44f840d77b16a0787
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-19 16:25:13 +0300

    `2-strncpy.c`

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,8 @@
 
 int _putchar(char character);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 
 
 #endif /*MAIN_H*/
