35ed08b667ea3f3533e3e7521f5081bd15c2b39a
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-23 18:29:20 +0300

    `1-memcpy.c`

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+# include "main.h"
+/**
+ * _memcpy - copy bytes from memory area
+ * to another one
+ * @src : copy from it 
+ * @dest : paste in it
+ */ 
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    while (n)
+    {
+        dest[n-1] = src[n-1];
+        n--;
+    }
+    return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
 
 int _putchar(char character);
 char *_memset(char *s, char b, unsigned int n);
-
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 
 #endif /*MAIN_H*/
