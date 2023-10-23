d8d0c11bd42be50ab566f164760f049a96caf2ef
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-23 19:42:59 +0300

    `4-strpbrk.c`

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
  * main - check the code
  *
@@ -9,10 +7,10 @@
 int main(void)
 {
     char *s = "hello, world";
-    char *f = "oleh";
-    unsigned int n;
+    char *f = "world";
+    char *t;
 
-    n = _strspn(s, f);
-    printf("%u\n", n);
+    t = _strpbrk(s, f);
+    printf("%s\n", t);
     return (0);
 }
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strpbrk - function locates the first occurrence in
+ * the string s of any of the bytes in the string accept
+ * @s:
+ * @accept:
+ * Return: pointer to the byte in s
+ * that matches one of the bytes in accept
+ * or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+return (s);
+}
+s++;
+}
+return (NULL);
+}
