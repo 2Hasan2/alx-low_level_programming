eab822159bdd282b8e2d9e93de5c6397e0deaa49
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-23 19:06:35 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,10 +9,12 @@
 
 char *_strchr(char *s, char c)
 {
-int i;
-for (i = 0; !((s[i] == c) && (s[i] == '\0')); i++)
-if (s[i] == c)
-return (s + i);
-else
-return (NULL);
+	int i;
+
+	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
+		;
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
 }
