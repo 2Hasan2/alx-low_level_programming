3a65ee8289f07c526ca6accd602e96ec690a1bee
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-23 20:04:25 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,14 +6,18 @@
  * Return: a pointer to the beginning of the located substring
  */
 
-char *_strstr(char *haystack, char *needle) {
-while (*haystack) {
+char *_strstr(char *haystack, char *needle)
+{
+while (*haystack)
+{
 int i = 0;
 char *word = haystack;
-while (haystack[i] == needle[i] && needle[i] != '\0') {
+while (haystack[i] == needle[i] && needle[i] != '\0')
+{
 i++;
 }
-if (needle[i] == '\0') {
+if (needle[i] == '\0')
+{
 return (word);
 }
 haystack++;
