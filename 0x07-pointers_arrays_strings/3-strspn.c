be86754394343d204333062739967970dc00ef74
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-23 19:33:24 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,7 @@
  * _strspn - function that gets the length of a prefix substring.
  * @s: the string
  * @accept: the accept characters
- * Returns: number of bytes from accept
+ * Return: number of bytes from accept
  */
 
 unsigned int _strspn(char *s, char *accept)
