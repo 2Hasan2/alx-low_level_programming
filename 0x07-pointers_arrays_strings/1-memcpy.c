a55db39b3b869e2c91bd9296b223b0f120c9752e
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-23 18:32:21 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,14 +4,15 @@
  * to another one
  * @src : copy from it 
  * @dest : paste in it
+ * @n : number of bytes
  */ 
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    while (n)
-    {
-        dest[n-1] = src[n-1];
-        n--;
-    }
-    return (dest);
+while (n)
+{
+dest[n-1] = src[n-1];
+n--;
+}
+return (dest);
 }
