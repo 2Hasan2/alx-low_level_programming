d8ecd1a6b304915cedb718f88cadcfaa0d1be8ec
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-25 17:55:04 +0300

    `1-print_rev_recursion.c`

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 int _putchar(char c);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 int factorial(int n);
 
 #endif /*MAIN_H*/
