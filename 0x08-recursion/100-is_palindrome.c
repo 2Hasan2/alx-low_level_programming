917c595d8eec1dcf722f98c662a480402fef58c6
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-25 20:54:02 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,20 +1,5 @@
 #include "main.h"
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to check
- * Return: 1 if @s is a palindrome, 0 otherwise
- */
-int is_palindrome(char *s)
-{
-int len = 0;
-int i = 0;
-int j = 0;
-while (s[i++])
-len++;
-j = len - 1;
-return (palindromeME(s, i, j));
-}
-/**
  * palindromeME - checks if a string is a palindrome
  * @s: string to check
  * @i: index
@@ -28,4 +13,19 @@ return (1);
 else if (s[i] != s[j])
 return (0);
 return (palindromeME(s, i + 1, j - 1));
+}
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to check
+ * Return: 1 if @s is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+int len = 0;
+int i = 0;
+int j = 0;
+while (s[i++])
+len++;
+j = len - 1;
+return (palindromeME(s, i, j));
 }
