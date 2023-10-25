f5b2aac6ffbf5815eac0ae6c5740a5d1910c8673
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-25 17:37:21 +0300

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,15 @@
  * @s: string to print
  * Return: void
  */
+
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
 _putchar('\n');
-}else{
+}else
+{
 _putchar(s[0]);
-_puts_recursion(s+1);
+_puts_recursion(s + 1);
 }
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <unistd.h>
-#include "_putchar.c"  
+#include "_putchar.c"
 
 void _puts_recursion(char *s);
 int factorial(int n);
