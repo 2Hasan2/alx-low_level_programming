056b77e501d5d8d38a44877456b14e9e8116be1f
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-25 18:02:28 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,7 +5,8 @@
  * Return: length of string
  */
 
-int _strlen_recursion(char *s){
+int _strlen_recursion(char *s)
+{
 int count = 0;
 if (*s == '\0')
 {
@@ -14,6 +15,6 @@ return (0);
 else
 {
 count = _strlen_recursion(s + 1);
-return count + 1;
+return (count + 1);
 }
 }
