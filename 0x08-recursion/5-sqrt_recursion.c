fdb21ef6710259ac1161f096de0a285fd700209d
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-25 19:29:39 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,7 +8,7 @@ int _sqrt_recursion(int n)
 {
 if (n == 0 || n == 1)
 return (n);
-return (sqrt( 0, n));
+return (sqrtME( 0, n));
 }
 /**
  * _sqrt - returns the square root of a number
@@ -16,11 +16,11 @@ return (sqrt( 0, n));
  * @x: squared number
  * Return: the square root of @n
  */
-int sqrt(int n, int x)
+int sqrtME(int n, int x)
 {
 if (n > x / 2)
 return (-1);
 else if (n * n == x)
 return (n);
-return (sqrt(n + 1, x));
+return (sqrtME(n + 1, x));
 }
