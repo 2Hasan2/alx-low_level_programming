be5d909a12924e3c293afccb149dd4eac2d7f412
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-25 18:00:32 +0300

    `2-strlen_recursion.c`

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to be counted
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s){
+int count = 0;
+if (*s == '\0'){
+return 0;
+}
+else{
+count = _strlen_recursion(s + 1);
+return count + 1;
+}
+}
