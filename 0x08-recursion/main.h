97b546837b506ac4e0dc0bbdb3170039c2493b4c
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-25 17:28:25 +0300

    `0-puts_recursion`

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stddef.h>
+#include <unistd.h>
 
+void _puts_recursion(char *s);
 int factorial(int n);
 
 
