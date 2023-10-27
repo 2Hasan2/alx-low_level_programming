6c3595c0d39eb81d91163def65d3a99d0d0e655b
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-27 20:55:43 +0200

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+
+c_files=$(ls *.c 2>/dev/null)
+
+if [ -z "$c_files" ]; then
+  exit 1
+fi
+
+for file in $c_files; do
+  gcc -c "$file"
+done
+
+ar rcs liball.a *.o
+
+rm -f *.o
+
+exit 0
