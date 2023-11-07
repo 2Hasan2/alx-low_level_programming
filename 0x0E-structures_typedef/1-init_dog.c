25d767be0f92cffd2be9c1d2ad37bd288ba18d6e
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-11-07 18:19:23 +0200

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,12 +9,11 @@
  *
  * Return: void
  */
-// use betty style
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d == NULL) // if d is NULL, return
-        return;
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
