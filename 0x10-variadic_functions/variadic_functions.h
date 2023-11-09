4cf0fc28ba780ba2d89aaf7d122c4880900c973e
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-11-09 19:56:56 +0200

    `3-print_all.c`

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,10 +3,20 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+typedef struct printer{
+char *symbol;
+void (*print)(va_list arg);
+} printer_t;
+
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
 
 #endif /* MAIN_H */
