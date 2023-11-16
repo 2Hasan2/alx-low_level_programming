91cc9e2cd0a2f9ee9638de7cbaf6c10a82a4ea17
2Hasan2 <133760904+2Hasan2@users.noreply.github.com>
2023-11-16 19:44:06 +0200

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,17 +6,5 @@
  */
 void __attribute__((constructor)) pre_main(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
-
-/**
- * main - The main function.
- * It prints a message.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
