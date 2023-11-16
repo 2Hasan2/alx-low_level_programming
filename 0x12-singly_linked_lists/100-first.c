72be5874e70fa7dc635813783a65735cc1d74b73
2Hasan2 <133760904+2Hasan2@users.noreply.github.com>
2023-11-16 19:47:52 +0200

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void __attribute__((constructor)) pre_main(void);
  */
 void pre_main(void)
 {
-printf("You're beat! and yet, you must allow,\n",
+printf("You're beat! and yet, you must allow,\n"
 "I bore my house upon my back!\n");
 }
