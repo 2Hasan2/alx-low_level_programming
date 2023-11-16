71b5a478ac78f070064ae3728b2c7e2a13047eb3
2Hasan2 <133760904+2Hasan2@users.noreply.github.com>
2023-11-16 19:45:18 +0200

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+void __attribute__((constructor)) pre_main(void);
+
 /**
  * pre_main - Function to be executed before main.
  * It prints the specified message.
  */
-void __attribute__((constructor)) pre_main(void)
+void pre_main(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
