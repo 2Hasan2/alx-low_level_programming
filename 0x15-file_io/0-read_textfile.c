7b683079d16e2777abfcdd6a7aaa62d393200ddf
2Hasan2 <133760904+2Hasan2@users.noreply.github.com>
2023-11-28 01:10:14 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,53 +1,40 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of the file to read
- * @letters: number of letters it should read and print
+ * @letters: number of letters to read and print
  *
- * Return: actual number of letters it could read and print, 0 if:
- * - the file can not be opened or read,
- * - filename is NULL,
+ * Return: number of letters read and printed, or 0 if:
+ * - filename is NULL
  * - write fails or does not write the expected amount of bytes
+ * - the file cannot be opened or read
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t read_bytes, write_bytes;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	/* open file */
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-
-	/* allocate buffer */
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	/* read file */
-	read_bytes = read(fd, buffer, letters);
-	if (read_bytes == -1)
-	{
-		free(buffer);
-		return (0);
-	}
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	/* write to POSIX standard output */
-	write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
-	if (write_bytes == -1 || read_bytes != write_bytes)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(fd);
+	close(o);
 
-	return (write_bytes);
+	return (w);
 }
