70fa6b829e802eabd5b8f539dc09b13c0d97d6fa
2Hasan2 <133760904+2Hasan2@users.noreply.github.com>
2023-11-28 00:58:56 +0200

    `1-create_file.c`

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and fills it with text
+ * @filename: name of the file to create
+ * @text_content: text to fill the file with
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, write_bytes, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		write_bytes = write(fd, text_content, i);
+		if (write_bytes == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
