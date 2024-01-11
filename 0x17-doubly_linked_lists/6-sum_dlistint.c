46df294c9d57de5de299d983c188ab750019e102
2Hasan2 <h01554176846@gmail.com>
2024-01-11 21:14:12 +0200

    6-fix betty

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t
+ * linked list
  * @head: pointer to head of list
  * Return: sum of all data (n) or 0 if list is empty
  */
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to pointer to head of list
+ * @idx: index of list where new node should be added
+ * @n: data to be added to new node
+ * Return: address of new node or NULL if failed
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *aux = NULL, *new_node = NULL;
+
+	if (!h)
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	aux = *h;
+	while (idx > 1)
+	{
+		if (!aux)
+			return (NULL);
+		aux = aux->next;
+		idx--;
+	}
+	if (!aux->next)
+		return (add_dnodeint_end(h, n));
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = aux;
+	new_node->next = aux->next;
+	aux->next->prev = new_node;
+	aux->next = new_node;
+	return (new_node);
+}
