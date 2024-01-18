ff3c9f285f58a3c63d9c38f208e592449928692f
2Hasan2 <h01554176846@gmail.com>
2024-01-18 14:42:56 +0200

    done

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: destination of string to be copied
+ * @src: source of string to be copied
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	*(dest + i) = *(src + i);
+
+	return (dest);
+}
